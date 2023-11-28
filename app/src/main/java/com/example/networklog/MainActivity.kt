cca7b8130273b01ff0a868ad55a9a07a104ade35
MuhammadMahedi <mahedi.hasan.23599@gmail.com>
2023-11-28 16:04:27 +0600

    Admin is not activate

---
+++ app/src/main/java/com/example/networklog/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.networklog
+
+import android.app.Activity
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.example.networklog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private var batchToken = 100L  //set the batch token here
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val devicePolicyManager = getSystemService(Activity.DEVICE_POLICY_SERVICE) as DevicePolicyManager?
+        val adminComponent = ComponentName(this, MyDeviceAdminReceiver::class.java)
+
+        binding.btnCheckEnabled.setOnClickListener {
+            if (devicePolicyManager != null) {
+                if (devicePolicyManager.isAdminActive(adminComponent)) {
+                    devicePolicyManager.setNetworkLoggingEnabled(adminComponent, true)
+                    val isEnable = devicePolicyManager.isNetworkLoggingEnabled(adminComponent)
+                    Toast.makeText(this@MainActivity, "$isEnable", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(this@MainActivity, "Admin is not Active", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "No Device Policy Manager found", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        binding.btnRetrieveLogs.setOnClickListener {
+            if (devicePolicyManager != null) {
+                if (devicePolicyManager.isAdminActive(adminComponent)) {
+                    val networkLogs = devicePolicyManager.retrieveNetworkLogs(componentName ,batchToken)
+                    Toast.makeText(this@MainActivity, "$networkLogs", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Admin is not Active", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "No Device Policy Manager found", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/networklog/MyDeviceAdminReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.networklog
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyDeviceAdminReceiver :  DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        // Perform actions when device admin is enabled
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+        // Perform actions when device admin is disabled
+    }
+}
